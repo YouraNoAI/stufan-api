4edcba82ea0964c0bbc09e795020ca0a349ccab8
Youra No AI <154102639+YouraNoAI@users.noreply.github.com>
2026-02-07 23:58:42 +0700

    commit

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,14 @@
-import mysql from "mysql2/promise";
+import serverlessMysql from "serverless-mysql";
 import dotenv from "dotenv";
 
 dotenv.config();
-export const db = mysql.createPool({
-  host: process.env.db_host,
-  user: process.env.db_user,
-  port: 3307,
-  password: process.env.db_password,
-  database: process.env.db_name,
-  multipleStatements: true,
+
+export const db = serverlessMysql({
+  config: {
+    host: process.env.db_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_name,
+    port: 3307,
+  }
 });
